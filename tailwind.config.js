aae445fce01f6bc3a01f610f831135c250ee645e
gersonDantas <gerson.accari@hotmail.com>
2021-08-26 18:49:01 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 module.exports = {
   purge: {
     content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}'
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
     ],
-    //includes all the classes I want 
+    //includes all the classes I want
     //to be present in the final version
     safelist: [
-      /^bg-/,
-      /^to-/,
-      /^from-/,
-      /^flex/,
-      /^item-/,
-      /^h-/,
-      /^m-/,
-      /^p-/,
-      /^bg-/,
-      /^text-/,
-      /^justify-/,
-      /^border-/,
-      /^rounded-/,
-      /^hover:/,
-      /^focus:/,
-    ]
+      /bg-*/,
+      /to-*/,
+      /from-*/,
+      /flex*/,
+      /item-*/,
+      /h-*/,
+      /m-*/,
+      /p-*/,
+      /bg-*/,
+      /text-*/,
+      /justify-*/,
+      /border-*/,
+      /rounded-*/,
+      /hover:*/,
+      /focus:*/,
+    ],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -32,4 +32,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
